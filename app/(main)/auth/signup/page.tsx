87d65aa687e6ae065d0d3b9788af76d69d29df00
Rohan-Peddamallu <rohanpeddamallu@gmail.com>
2024-12-25 21:33:08 -0600

    Added logo

--- app/(main)/auth/signup/page.tsx
+++ app/(main)/auth/signup/page.tsx
@@ -5,7 +5,18 @@ import Image from "next/image";
 export default function SignUpPage() {
   return (
     <div className="min-h-screen flex overflow-hidden relative">
-      <div className="w-1/2 bg-white rounded-tr-[50px] rounded-br-[50px] relative z-10"></div>
+      <div className="w-1/2 bg-white rounded-tr-[50px] rounded-br-[50px] relative z-10">
+        <div className="p-8">
+          <Image
+            src="/linkerr.png"
+            alt="Linkerr Logo"
+            width={150}
+            height={50}
+            className="object-contain"
+            priority
+          />
+        </div>
+      </div>
 
       <div className="w-full h-full absolute" style={{ background: "#A2B0FF" }}>
         <div className="flex justify-end h-screen">
