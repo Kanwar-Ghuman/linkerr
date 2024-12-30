84bca3506f63ed593e1954c78497d788137cf038
Rohan-Peddamallu <rohanpeddamallu@gmail.com>
2024-12-29 22:05:16 -0600

    This is where iphone and desktop are responsive but curve is gone

--- app/(main)/auth/login/page.tsx
+++ app/(main)/auth/login/page.tsx
@@ -178,6 +178,7 @@ export default function SignUpPage() {
                   Sign in with Google
                 </Button>
               </div>
+
               <div className="text-center md:-ml-64 text-sm md:text-base">
                 <span className="text-gray-600">Don't have an account? </span>
                 <Link
