542f5558df64f77bcf927296c855069a9bb8febb
Rohan-Peddamallu <rohanpeddamallu@gmail.com>
2025-01-23 14:36:40 -0600

    Added routes for student and employer'

---
+++ app/employer/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+export default function EmployerPage() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">Hello World</h1>
+      <Link href="/" className="text-blue-600 hover:text-blue-800">
+        Back to Home
+      </Link>
+    </div>
+  );
+}
