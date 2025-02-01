a012bc9443e32e7084283a48a330b5b8cc6a9e51
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2025-01-31 23:03:14 -0600

    work

--- app/api/jobs/route.ts
+++ app/api/jobs/route.ts
@@ -8,48 +8,47 @@ import { auth } from "@/auth";
 const prisma = new PrismaClient();
 
 export async function POST(request: Request) {
-    try {
-        const body = await request.json();
-        const session = await auth()
+  try {
+    const body = await request.json();
+    const session = await auth();
 
-        const employer = await prisma.employer.findUnique({
-            where: {
-                id: session?.user.id
-            }
-        })
+    const employer = await prisma.employer.findUnique({
+      where: {
+        id: session?.user.id,
+      },
+    });
 
-        console.log(employer)
+    console.log(employer);
 
-        // Validate form data
-        const validation = validateForm(JobValidation, body);
-        if (!validation.isValid) return validation.error;
+    // Validate form data
+    const validation = validateForm(JobValidation, body);
+    if (!validation.isValid) return validation.error;
 
-        const jobData = validation.data;
-        const remote = jobData.remote.toUpperCase();
+    const jobData = validation.data;
+    const remote = jobData.remote.toUpperCase();
 
-        const createdJob = await prisma.job.create({
-            data: {
-                jobTitle: jobData.jobTitle,
-                jobDescription: jobData.jobDescription,
-                jobType: jobData.jobType || null,
-                roleLocation: jobData.roleLocation || "",
-                companyName: jobData.companyName,
-                remote: remote as "REMOTE" | "ONSITE" | "HYBRID",
-                skills: jobData.skills,
-                pay: jobData.pay,
-                employer,
-                employerId: session?.user.id, // Replace with real auth
-            },
-        });
+    const createdJob = await prisma.job.create({
+      data: {
+        jobTitle: jobData.jobTitle,
+        jobDescription: jobData.jobDescription,
+        jobType: jobData.jobType || null,
+        roleLocation: jobData.roleLocation || "",
+        companyName: jobData.companyName,
+        remote: remote as "REMOTE" | "ONSITE" | "HYBRID",
+        skills: jobData.skills,
+        pay: jobData.pay,
+        employerId: session?.user.id, // Replace with real auth
+      },
+    });
 
-        return NextResponse.json(createdJob, { status: 201 });
-    } catch (error) {
-        console.error("Error creating job:", error);
-        return NextResponse.json(
-            { error: "Internal Server Error" },
-            { status: 500 }
-        );
-    }
+    return NextResponse.json(createdJob, { status: 201 });
+  } catch (error) {
+    console.error("Error creating job:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
 
 export async function GET(request: Request) {
